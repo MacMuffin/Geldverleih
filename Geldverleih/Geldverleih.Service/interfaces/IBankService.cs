856fee87d33fb3414a4cf0b598e693f8712ca0ec
MacMuffin <andy_tarakanov@web.de>
2012-06-18 18:27:33 +0200

    - Service Projekt erstellt. - interface für ausleihen und einzahlen erstellt.

---
+++ Geldverleih/Geldverleih.Service/interfaces/IBankService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Geldverleih.Domain;
+
+namespace Geldverleih.Service
+{
+    public interface IBankService
+    {
+        void GeldAusleihen(Kunde kunde, VerleihKondition verleihKondition);
+        void GeldEinzahlen(Kunde kunde, decimal betrag);
+    }
+}
