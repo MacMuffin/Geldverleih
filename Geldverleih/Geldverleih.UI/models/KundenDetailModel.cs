0efd56a3e95c27b81a53f2ae8502df3d787ea226
MacMuffin <andy_tarakanov@web.de>
2012-06-20 08:54:05 +0200

    - KundenDetailModel und KundenModel erstellt, zur Darstellung von Kunden.

---
+++ Geldverleih/Geldverleih.UI/models/KundenDetailModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Geldverleih.UI.models
+{
+    public class KundenDetailModel
+    {
+        public string Vorname { get; set; }
+        public string Name { get; set; }
+        public string Wohnort { get; set; }
+        public string Adresse { get; set; }
+        public int PLZ { get; set; }
+    }
+}
---
+++ Geldverleih/Geldverleih.UI/models/KundenModel.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using Geldverleih.Domain;
+
+namespace Geldverleih.UI.models
+{
+    public class KundenModel
+    {
+        public IList<Kunde> KundenListe { get; set; }
+    }
+}
